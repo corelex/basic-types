4e7910ee24ca73d5615a9a8f9b5472f7ac859cc2
sheetskristen <sheetskristen@gmail.com>
2019-09-22 23:45:26 -0400

    refactored the positional and optional arguments for improved readbility. added two new cluster modes: word-cluster and polysem-cluster. added the ability to use all cluster models on all and test data sets. added the ability to create word-embeddings from the full dataset. improved color mapping for all cluster plots.

--- README.md
+++ README.md
@@ -1,42 +1,65 @@
 # Basic Types Classification
 ## Approach
 CoreLex and CoreLex 2.0 investigate systematic polysemy in nouns, establishing an ontology and semantic database of 126 semantic types, defining a large number of systematic polysemous classes through an analysis of WordNet sense distributions. 
+
 In order to better understand these sense distributions and the correlations between them, the ability to associate a lexical item with its semantic type automatedly would be of use. By using the SemCor corpus which comprised of texts semantically annotated with their WordNet sense. We are working to build a classifier that can correctly predict a lexical item’s semantic type in novel texts. 
 
+There are additional models for use in the visual analysis of basic semantic types. 
 
 ## Running the Code
 The module for running the classifier is basic-types.py. Requirements for running this are Stanford's CoreNLP and the packages specified in requirements.txt.
 
-Additional notes on usage:
+**Additional Notes on Usage**
 
-$ python3 basic-types.py --train-semcor
+ python3 basic-types.py train-semcor
 
     Train a classifier from all of Semcor and save it in
-    ../data/classifier-all.pickle.
+    ../data/classifier-all.pickle. Use flag -create-embedding to save new word embeddings for this model.
+    Default off.
 
-$ python3 basic-types.py --train-test
+$ python3 basic-types.py train-test
 
-    Train a classifier from a fragemtn of Semcor (two files) and save it in
+    Train a classifier from a fragment of Semcor (two files) and save it in
     ../data/classifier-002.pickle, for testing and debugging purposes.
+    Use flag -create-embedding to save new word embeddings for this model.
+    Default off.
 
-$ python3 basic-types.py --test
+$ python3 basic-types.py test
 
     Test the classifier on a feature set and test the evaluation code.
 
-$ python3 basic-types.py --classify-file FILENAME
+$ python3 basic-types.py classify-file -input-file FILENAME
 
-    Run the classifier on filename, output will be written to the terminal.
+    Run the classifier on filename, output will be written to the terminal. Specify the file with the -f option.
 
-$ python3 basic-types.py --classify-spv1
+$ python3 basic-types.py classify-spv1
 
     Run the classifier on all SPV1 files, output will be written to the out/
     directory.
 
-The last two invocations both require the NLTK CoreNLPDependencyParser which
+$ python3 basic-types.py cluster
+
+    Creates a t-distributed stochastic neighbor embedding model using the created word embeddings (see: train-semcor
+    and train-test modes). Use the -model test option to use the test-sized model. Default full (all) model.
+
+$ python3 basic-types.py word-cluster
+
+    Creates a t-distributed stochastic neighbor embedding model using the created word embeddings (see: train-semcor
+    and train-test modes) for specific words in the corpus. Use the -w WORD option to specify an input word.
+    Use the -model test option to use the test-sized model. Default full (all)
+    model.
+
+$ python3 basic-types.py polysem-cluster
+
+    Creates a t-distributed stochastic neighbor embedding model using the created word embeddings (see: train-semcor
+    and train-test modes) for lemmas associate with multiple basic types in the corpus. Use the -model test option to
+    use the test-sized model. Default full (all) model.
+
+
+The "classify-file" and "classify-spv1" invocations both require the NLTK CoreNLPDependencyParser which
 assumes that the Stanford CoreNLP server is running at port 9000. To use the
 server run the following from the corenlp directory:
 
 $ java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -preload tokenize,ssplit,pos,lemma,depparse -status_port 9000 -port 9000 -timeout 15000
 
-Note that this invocation does not allow you browser access to port 9000 because
-the homepage uses an annotator that is not loaded by the above command.
+Note that this invocation does not allow you browser access to port 9000 because the homepage uses an annotator that is not loaded by the above command.
--- code/basic-types.py
+++ code/basic-types.py
@@ -6,35 +6,52 @@ Module for creating and running the basic types classifier.
 
 Usage
 
-$ python3 basic-types.py --train-semcor
+$ python3 basic-types.py train-semcor
 
     Train a classifier from all of Semcor and save it in
-    ../data/classifier-all.pickle.
+    ../data/classifier-all.pickle. Use flag -create-embedding to save new word embeddings for this model.
+    Default off.
 
-$ python3 basic-types.py --train-test
+$ python3 basic-types.py train-test
 
     Train a classifier from a fragment of Semcor (two files) and save it in
     ../data/classifier-002.pickle, for testing and debugging purposes.
+    Use flag -create-embedding to save new word embeddings for this model.
+    Default off.
 
-$ python3 basic-types.py --test
+$ python3 basic-types.py test
 
     Test the classifier on a feature set and test the evaluation code.
 
-$ python3 basic-types.py --cluster
+$ python3 basic-types.py classify-file -input-file FILENAME
 
-    Creates a t-distributed stochastic neighbor embedding model using the created word embeddings. Currently only
-    available for the train-test model.
+    Run the classifier on filename, output will be written to the terminal. Specify the file with the -f option.
 
-$ python3 basic-types.py --classify-file FILENAME
-
-    Run the classifier on filename, output will be written to the terminal.
-
-$ python3 basic-types.py --classify-spv1
+$ python3 basic-types.py classify-spv1
 
     Run the classifier on all SPV1 files, output will be written to the out/
     directory.
 
-The last two invocations both require the NLTK CoreNLPDependencyParser which
+$ python3 basic-types.py cluster
+
+    Creates a t-distributed stochastic neighbor embedding model using the created word embeddings (see: train-semcor
+    and train-test modes). Use the -model test option to use the test-sized model. Default full (all) model.
+
+$ python3 basic-types.py word-cluster
+
+    Creates a t-distributed stochastic neighbor embedding model using the created word embeddings (see: train-semcor
+    and train-test modes) for specific words in the corpus. Use the -w WORD option to specify an input word.
+    Use the -model test option to use the test-sized model. Default full (all)
+    model.
+
+$ python3 basic-types.py polysem-cluster
+
+    Creates a t-distributed stochastic neighbor embedding model using the created word embeddings (see: train-semcor
+    and train-test modes) for lemmas associate with multiple basic types in the corpus. Use the -model test option to
+    use the test-sized model. Default full (all) model.
+
+
+The "classify-file" and "classify-spv1" invocations both require the NLTK CoreNLPDependencyParser which
 assumes that the Stanford CoreNLP server is running at port 9000. To use the
 server run the following from the corenlp directory:
 
@@ -44,15 +61,18 @@ Note that this invocation does not allow you browser access to port 9000 because
 the homepage uses an annotator that is not loaded by the above command.
 
 """
-import os, sys, csv, getopt, pickle, codecs, json, glob
-from tqdm import tqdm
+import argparse, codecs, csv, glob, json, pickle, os, sys
+from collections import Counter
+
+from bert_embedding import BertEmbedding
+import matplotlib.pyplot as plt, matplotlib.cm as cm, matplotlib.patches as mpatches
 import nltk
 from nltk.parse.corenlp import CoreNLPDependencyParser
 from nltk.stem import WordNetLemmatizer
+import numpy as np
 from semcor import Semcor, SemcorFile
-from bert_embedding import BertEmbedding
 from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 SC_SENT = '../data/semcor.sent.tsv'
@@ -106,7 +126,7 @@ def extract_types(sc):
     return types, mapping
 
 
-def feature_set(types, token_features, mapping, bert_embeddings):
+def feature_set(types, token_features, mapping):
     mapped = zip(token_features, types)
     feature_set = []
     for i in mapped:
@@ -114,13 +134,14 @@ def feature_set(types, token_features, mapping, bert_embeddings):
             features = i[0]
             # indexs: [0] token_id, [1] sent_id, [2] token_no, [3] surface,[4] lemma, [5] pos, [6] sense_no, [7] sense_key, [8] ssid,
             # [9] int_dom_token_no, [10] dom_token_id, [11] rel
-            sentence_embedding = bert_embeddings[features[1]]
-            if features[5] != 'VB'and type(sentence_embedding[int(features[2])-1][1]) is not str:
+            #sentence_embedding = bert_embeddings[features[1]]
+            #if features[5] != 'VB'and type(sentence_embedding[int(features[2])-1][1]) is not str:
+            if features[5] != 'VB':
                 feature_dict = {
                     "surface" : features[3],
                     "lemma" : features[4],
                     "pos" : features[5],
-                    "embedding_vector" : sentence_embedding[int(features[2])-1][1][0].tostring(),
+                    # "embedding_vector" : sentence_embedding[int(features[2])-1][1][0].tostring(),
                     # "sense_no" : features[6],
                     # "sense_key" : features[7],
                     # "ssid" : features[8],
@@ -242,14 +263,13 @@ def sent_count(model_name):
 def _train(semcor, features_in, model_name, create_embeddings):
     token_features, sentences = extract_token_tsv_data(features_in)
     if create_embeddings:
-        bert_embeddings = create_bert_embeddings(sentences, model_name)
-    else:
-        bert_embeddings = load_embeddings(model_name)
+         create_bert_embeddings(sentences, model_name)
+    # else:
+    #     load_embeddings(model_name)
     types_from_semcor, identifier2token = extract_types(semcor)
     print_type_count(types_from_semcor)
-    feature_data = feature_set(types_from_semcor, token_features, identifier2token, bert_embeddings)
+    feature_data = feature_set(types_from_semcor, token_features, identifier2token)
     training_set, test_set = split_data(feature_data)
-    # maybe add an option to train on the entire set
     classifier = train_classifier(training_set)
     print("Labels: %s" % classifier.labels())
     accuracy = evaluate_classifier(classifier, test_set)
@@ -333,87 +353,228 @@ def run_classifier_on_spv1():
         except:
             print('ERROR')
 
-def cluster(model_name):
+
+def tokentypevector(model_name):
     token_features, sentences = extract_token_tsv_data(SC_TOKEN_FT_SMALL)
     bert_embeddings = load_embeddings(model_name)
-    types_from_semcor, identifier2token = extract_types(Semcor(2))
+    if model_name == '002':
+        semcor = Semcor(2)
+    else:
+        semcor = Semcor()
+    types_from_semcor, identifier2token = extract_types(semcor)
     mapping = zip(token_features, types_from_semcor)
     type2vector = []
     for i in mapping:
         if i[1] is not None and len(i[1])<4:
             features = i[0]
             sentence_embedding = bert_embeddings[features[1]]
-            type2vector.append((i[1], i[0][3], sentence_embedding[int(features[2])-1][1][0]))
-    tsne_plot(type2vector)
+            type2vector.append((i[1], i[0][4], i[0][3], sentence_embedding[int(features[2])-1][1][0]))
 
+    return type2vector
 
-def tsne_plot(type2vector):
+def tsne_all_prep(type2vector):
     labels = []
-    tokens = []
+    token_vectors = []
     surface_form = []
 
-    for (b_type, surface, vector) in type2vector:
+    for (b_type, lemma, surface, vector) in type2vector:
         if type(vector) is not str:
-            tokens.append(vector)
+            token_vectors.append(vector)
             labels.append(b_type)
             surface_form.append(surface)
 
-    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
-    new_values = tsne_model.fit_transform(tokens)
+    return labels, token_vectors, surface_form
 
+def lemma_prep(type2vector, lemma):
+    labels = []
+    token_vectors = []
+    surface_form = []
+
+    for (b_type, cur_lemma, surface, vector) in type2vector:
+        if type(vector) is not str and lemma == cur_lemma:
+            token_vectors.append(vector)
+            labels.append(b_type)
+            surface_form.append(surface)
+
+    return labels, token_vectors, surface_form
+
+
+def polysem_prep(type2vector):
+    all_types = {}
+    for (b_type, cur_lemma, surface, vector) in type2vector:
+        if type(vector) is not str:
+            if cur_lemma not in all_types:
+                all_types.update({cur_lemma:
+                                      {'b_type':[b_type],
+                                       'instances': [
+                                           {'surface': surface,
+                                            'bert_vector': vector,
+                                            'b_type': b_type}]
+                                       }
+                                  })
+            else:
+                all_types[cur_lemma]['b_type'].append(b_type)
+                all_types[cur_lemma]['instances'].append({'surface': surface,
+                                                          'bert_vector': vector,
+                                                          'b_type': b_type})
+        polysem_labels = []
+        polysem_token_vectors = []
+        polysem_surface_form = []
+        for (entry, values) in all_types.items():
+            uniq_types = list(set(values['b_type']))
+            if len(uniq_types) > 1:
+                for instance in values['instances']:
+                    polysem_labels.append(instance['b_type'])
+                    polysem_token_vectors.append(instance['bert_vector'])
+                    polysem_surface_form.append(instance['surface'])
+
+    return polysem_labels, polysem_token_vectors, polysem_surface_form
+
+
+def model_cluster(model_name):
+    type2vector = tokentypevector(model_name)
+    labels, vectors, surface_form = tsne_all_prep(type2vector)
+    x, y = create_tsne_model(vectors)
+    cmap, c_handles =  create_color_handles(labels)
+    display_full_plot(x, y, cmap, c_handles, labels)
+
+def polysem_cluster(model_name):
+    type2vector = tokentypevector(model_name)
+    labels, vectors, surface_form = polysem_prep(type2vector)
+    labeled_cluster_output(labels, vectors, surface_form)
+
+
+def word_cluster(model_name, word):
+    type2vector = tokentypevector(model_name)
+    lemmatizer = WordNetLemmatizer()
+    lemma = lemmatizer.lemmatize(word)
+    labels, vectors, surface_form = lemma_prep(type2vector, lemma)
+    print("Input word: ", word)
+    labeled_cluster_output(labels, vectors, surface_form)
+
+
+def labeled_cluster_output(labels, vectors, surface_form):
+    associated_forms = Counter(surface_form)
+    associated_btypes = Counter(labels)
+    print("Associated word forms: ")
+    for (form, count) in associated_forms.items():
+        print("Form: ", form, " Count: ", count)
+    print("Associated basic types: ")
+    for (btype, count) in associated_btypes.items():
+        print("Basic type: ", btype, " Count: ", count)
+    if surface_form:
+        x, y = create_tsne_model(vectors)
+        cmap, c_handles = create_color_handles(labels)
+        display_word_plot(x, y, cmap, c_handles, labels, surface_form)
+    else:
+        print("Error: this token does not occur in the corpus.")
+
+
+def create_tsne_model(token_vectors):
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    print("Fitting TSNE model...")
+    new_values = tsne_model.fit_transform(token_vectors)
     x = []
     y = []
     for value in new_values:
         x.append(value[0])
         y.append(value[1])
 
-    plt.figure(figsize=(10, 10))
+    return x,y
 
-    colors = ['red', 'lightcoral', 'darkgreen', 'limegreen', 'darkorange', 'bisque', 'gold', 'yellow', 'fuchsia',
-              'black', 'gray', 'deeppink', 'pink', 'blueviolet', 'mediumblue', 'aquamarine', 'cyan', 'darkslategrey', 'lawngreen', 'lightskyblue', 'sandybrown', 'teal']
-    cmap = {}
-    for i in range(len(set(labels))):
-        cmap.update({list(set(labels))[i]: colors[i]})
-
-    print(cmap)
+def display_full_plot(x, y, cmap, mpatch_handles, labels):
+    print("Generating plot...")
+    plt.figure(figsize=(16, 16))
 
     for i in range(len(x)):
-        plt.scatter(x[i], y[i], c=cmap[labels[i]])
+        plt.scatter(x[i], y[i], c=[cmap[labels[i]]])
+
+    plt.legend(handles=mpatch_handles)
+    plt.show()
+
+def display_word_plot(x, y, cmap, mpatch_handles, labels, surface_form):
+    print("Generating plot...")
+    plt.figure(figsize=(16, 16))
+
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i], c=[cmap[labels[i]]])
         plt.annotate(surface_form[i],
                      xy=(x[i], y[i]),
                      xytext=(5, 2),
                      textcoords='offset points',
                      ha='right',
                      va='bottom')
+
+    plt.legend(handles=mpatch_handles)
     plt.show()
 
+def create_color_handles(labels):
+    colormap = cm.colors.ListedColormap(cm.get_cmap('gist_ncar')(np.linspace(0,1,len(set(labels))+1)))
+    cmap = {}
+    for i in range(len(set(labels))):
+        cmap.update({list(set(labels))[i]: colormap.colors[i]})
+    mpatch_handles = []
+    for (btype, color) in cmap.items():
+        mpatch_handles.append(mpatches.Patch(color=color, label=btype))
+
+    return cmap, mpatch_handles
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode', choices=['train-test',
+                                         'train-semcor',
+                                         'test',
+                                         'classify-file',
+                                         'classify-spv',
+                                         'cluster',
+                                         'word-cluster',
+                                         'polysem-cluster'])
+    parser.add_argument('-create-embeddings',
+                        action='store_true',
+                        default=False,
+                        help="sets create new embeddings in training to true")
+    parser.add_argument('-model',
+                        default='all',
+                        choices=['all', 'test'],
+                        help="choose a model to run against")
+    parser.add_argument('-input-file',
+                        default=None,
+                        help="input file for classify-file mode")
+    parser.add_argument('-w',
+                        default=None,
+                        type=str,
+                        help="word input for word cluster model")
+    args = parser.parse_args()
+    mode = args.mode
+    if args.model == "test":
+        model = "002"
+    else:
+        model = args.model
 
-    options = ['train-test', 'train-semcor', 'test', 'classify-file=', 'classify-spv1','cluster']
-    opts, args = getopt.getopt(sys.argv[1:], '', options)
-
-    for opt, val in opts:
-        #TODO create options for creating/not creating embeddings
-        if opt == '--train-test':
-            train_test(False)
-
-        elif opt == '--train-semcor':
-            train(True)
-
-        elif opt == '--test':
-            # test the classifier with the full model on the 002 test set, gives
-            # unrealistic results because the training data probably includes
-            # the test data, just here to see whether the mechanism works
-            test_classifier('all', '002')
-
-        elif opt == '--classify-file':
-            filename = val
+    if mode == 'train-test':
+        train_test(args.creat_embeddings)
+    elif mode == 'train-semcor':
+        train(args.create_embeddings)
+    elif mode == 'test':
+        #     test the classifier with the full model on the 002 test set, gives
+        #     unrealistic results because the training data probably includes
+        #     the test data, just here to see whether the mechanism works
+        test_classifier('all', '002')
+    elif mode == 'classify-file':
+        filename = args.input_file
+        if filename:
             run_classifier_on_file(filename)
-
-        elif opt == '--classify-spv1':
-            run_classifier_on_spv1()
-
-        elif opt == '--cluster':
-            cluster('002')
-
+        else:
+            print("Error: No input file provided.")
+    elif mode == 'classify-spv':
+        run_classifier_on_spv1()
+    elif mode == 'cluster':
+        model_cluster(model)
+    elif mode == 'word-cluster':
+        word = args.w
+        if word:
+            word_cluster(model, word)
+        else:
+            print("Error: No input word provided.")
+    elif mode == 'polysem-cluster':
+        polysem_cluster(model)
